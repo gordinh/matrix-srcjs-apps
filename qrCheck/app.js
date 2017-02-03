b9e4c2e22cea8b3d61a07995f20d8a23c0a6a902
Lucas <lucas.ecomp2012@gmail.com>
2017-02-03 14:16:18 -0300

    standby

--- qrCheck/app.js
+++ qrCheck/app.js
@@ -2,3 +2,27 @@
 // matrix.init()....
 //
 // have fun
+
+
+matrix.led('red').render();
+
+matrix.init('camera').then(function(data){
+
+	var QrCode = require('qrcode-reader');
+	var qr = new QrCode();
+
+	qr.callback = function(result,err) {
+		if(result)
+			console.log(result)
+	}
+
+	qr.decode(data);
+
+	// fonte:https://www.npmjs.com/package/qrcode-reader
+
+	// console.log('>>>>>>>>>>', data);
+	// matrix.led('green').render();
+	// setTimeout(function() {
+	// 	matrix.led('black').render();
+	// },2000);
+});
