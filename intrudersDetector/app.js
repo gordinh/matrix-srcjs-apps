41c538bf7398eecee3a75d5b277cbcdc10fdf137
carla <carla.ecomp@gmail.com>
2017-02-08 16:48:34 -0200

    incluide intruders detector

---
+++ intrudersDetector/app.js
@@ -0,0 +1,25 @@
+// app code goes here
+// matrix.init()....
+//
+// have fun
+
+var RaspiCam = require("../lib/raspicam");
+
+// Verificar horario
+matrix.init('face').then(function(data){
+	
+  	console.log('>>>>>>>>>>', data);
+  	var camera = new RaspiCam({
+		mode: "photo",
+		output: "./photo/image.jpg"+"time",
+		encoding: "jpg",
+		timeout: 0 // take the picture immediately
+	});
+
+	camera.on("start", function( err, timestamp ){
+		console.log("photo started at " + timestamp );
+	});
+
+	camera.start();
+});
+
